442a6aa9a46610e4f8ac3b02e4252f57b065bbd0
Supanut1911 <ynotmela@gmail.com>
2023-08-06 23:43:41 +0700

    feat: add components
    BREAKING CHANGE: n
    
    Closes n

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Alias
+        exercitationem placeat maxime ratione, odit reprehenderit aspernatur
+        temporibus deserunt tempora vero iusto sunt dolorem suscipit hic
+        voluptatem quidem laborum consequuntur accusantium.
+      </p>
+      {/* feed */}
+    </section>
+  );
 };
 
 export default Home;
