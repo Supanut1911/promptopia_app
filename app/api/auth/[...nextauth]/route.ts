06a3c4bc1704077c275341c7cd9538386dd5bde5
Supanut1911 <ynotmela@gmail.com>
2023-08-09 23:36:16 +0700

    fix: type of session by adding custom session send session.user.id to creator argument

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,8 +3,6 @@ import GoogleProvider from "next-auth/providers/google";
 
 import { connectToDB } from "@utils/database";
 import User from "@models/user";
-import { NextAuthOptions } from "next-auth";
-import { log } from "console";
 
 const handler = NextAuth({
   providers: [
@@ -47,56 +45,3 @@ const handler = NextAuth({
 });
 
 export { handler as GET, handler as POST };
-
-// export const authOptions: NextAuthOptions = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: `${process.env.GOOGLE_ID}`,
-//       clientSecret: `${process.env.GOOGLE_CLIENT_SECRET}`,
-//     }),
-//   ],
-//   callbacks: {
-//     // session: async ({ session }: any) => {
-//     //   console.log("called");
-//     //   const sessionUser = await User.findOne({ email: session.user.email });
-//     //   console.log("sessionUser", sessionUser);
-//     //   session.user.id = sessionUser._id.toString();
-//     //   return session;
-//     // },
-//     session: ({ session }: any) => {
-//       console.log("session =>", session);
-//     },
-
-//     jwt: ({ token, user }: any) => {
-//       console.log("token, => ", token);
-//     },
-
-//     async signIn({ profile }: any) {
-//       try {
-//         console.log("hiiiii signin");
-
-//         await connectToDB();
-
-//         //check if user already exist
-//         const userExists = await User.findOne({ email: profile.email });
-
-//         //if not, create a new user
-//         if (!userExists) {
-//           await User.create({
-//             email: profile.email,
-//             username: profile.name.replace(" ", "").toLowerCase(),
-//             image: profile.picture,
-//           });
-//         }
-
-//         return true;
-//       } catch (error) {
-//         console.log(error);
-//         return false;
-//       }
-//     },
-//   },
-// };
-
-// const handler = NextAuth(authOptions);
-// export { handler as GET, handler as POST };
