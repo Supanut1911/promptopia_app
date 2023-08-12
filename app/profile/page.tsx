22e8db3215995c10ebd192089f26098895b73e3d
Supanut1911 <ynotmela@gmail.com>
2023-08-12 16:23:29 +0700

    feat: Myprofile page
    BREAKING CHANGE: n
    
    Closes n

---
+++ app/profile/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState([]);
+
+  const handleEdit = async () => {};
+
+  const handleDelete = async () => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/:${session?.user.id}/posts`);
+      const data = await response.json();
+      setPosts(data);
+    };
+
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to ur personalize profile page"
+      data={posts}
+      handleEdit={() => handleEdit()}
+      handleDelete={() => handleDelete()}
+    />
+  );
+};
+
+export default MyProfile;
