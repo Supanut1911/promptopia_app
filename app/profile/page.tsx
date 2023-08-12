76b5535c3cb95c8e4447932e0a455602abd05f6f
Supanut1911 <ynotmela@gmail.com>
2023-08-13 00:23:14 +0700

    feat: implement DELETE prompt
    BREAKING CHANGE: n
    
    Closes n

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
 
 import Profile from "@components/Profile";
+import { log } from "console";
 
 const MyProfile = () => {
   const { data: session } = useSession();
@@ -15,7 +16,23 @@ const MyProfile = () => {
     router.push(`/update-prompt?id=${post._id}`);
   };
 
-  const handleDelete = async () => {};
+  const handleDelete = async (post: any) => {
+    console.log("handleDelete called");
+
+    const hasConfirmed = confirm(
+      "Are you sure you want to delete this prompt?"
+    );
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
+
+        const filteredPosts = posts.filter((p: any) => p._id !== post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
