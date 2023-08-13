39de6fa2a983806af0889f6ca74e1a1a1f4e9a4c
Supanut1911 <ynotmela@gmail.com>
2023-08-13 15:08:25 +0700

    feat: display profile from click profilt in promptCar

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -29,10 +29,20 @@ const PromptCard = ({
     setTimeout(() => setCoied(""), 3000);
   };
 
+  const handleClickProfile = () => {
+    if (post.creator._id === session?.user.id) return router.push("/profile");
+    router.push(
+      `/profile/${post.creator._id}?name=${post.creator.username}&mode="test"`
+    );
+  };
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+        <div
+          className="flex-1 flex justify-start items-center gap-3 cursor-pointer"
+          onClick={handleClickProfile}
+        >
           <Image
             src={post.creator.image}
             alt="user_image"
